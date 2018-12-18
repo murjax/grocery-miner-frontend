0046e81eea5a0d47d4db8033892d3dcc19cd3a8c
Ryan Murphy <murjax@gmail.com>
2018-12-17 22:14:29 -0500

    Allow form to add multiple items

--- app/controllers/item/new.js
+++ app/controllers/item/new.js
@@ -1,8 +1,27 @@
 import Controller from '@ember/controller';
 import moment from 'moment';
+import { set } from '@ember/object';
 
 export default Controller.extend({
+  draftItems: [],
+  reload() {
+    // this.get('model.draftItems').forEach(function(record) {
+    //   record.reload();
+    // });
+  },
+
   actions: {
+    addItem() {
+      const item = this.store.createRecord('item', {
+        name: this.name,
+        price: this.price,
+        purchaseDate: (moment(this.purchaseDate).format('YYYY-MM-DD'))
+      });
+      this.draftItems.pushObject(item);
+      this.model.draftItems.pushObject(item);
+      // set(this, 'model.draftItems', this.draftItems);
+      this.reload();
+    },
     onSubmit() {
       const item = this.store.createRecord('item', {
         name: this.name,

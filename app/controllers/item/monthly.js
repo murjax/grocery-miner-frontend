f6b47127d95e596e8c7b48eaebcb3b27cf6793e0
Ryan Murphy <murjax@gmail.com>
2018-12-26 17:16:33 -0500

    Add month and year selectors

--- app/controllers/item/monthly.js
+++ app/controllers/item/monthly.js
@@ -1,6 +1,30 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import moment from 'moment';
 
 export default Controller.extend({
+  queryParams: ['month', 'year'],
+
+  months: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'],
+  years: computed(function() {
+    const currentYear = new Date().getFullYear();
+    var startYear = 1995;
+    const years = [];
+
+    while (startYear <= currentYear) {
+      years.push(startYear++);
+    }
+    return years;
+  }),
+
+  month: computed(function() {
+    return moment().format('MM');
+  }),
+
+  year: computed(function() {
+    return new Date().getFullYear();
+  }),
+
   columns: [
     {
       name: 'Name',
--- app/routes/item/monthly.js
+++ app/routes/item/monthly.js
@@ -4,9 +4,15 @@ import config from '../../config/environment';
 
 export default Route.extend({
   ajax: inject('ajax'),
+  queryParams: {
+    month: { refreshModel: true },
+    year: { refreshModel: true }
+  },
 
   model(params) {
-    return this.get('ajax').request(`${config.host}/items/monthly`).then((response) => {
+    return this.get('ajax').request(`${config.host}/items/monthly`, {
+      data: { month: params.month, year: params.year }
+    }).then((response) => {
     return response;
     }).catch((error) => {
       return [];
